8f5c32752b6aa788d3f3620b33c48755a2639672
lpite <farionsasa@gmail.com>
2022-01-29 20:50:00 +0200

    2 фикс для vercel

--- config.ts
+++ config.ts
@@ -1,4 +1,6 @@
 export let APIURL = "https://quil-api.herokuapp.com";
-if (window && window.location.href === "http://localhost:8080/") {
-  APIURL = "http://localhost:3000";
+if (typeof window !== "undefined") {
+  if (window.location.href === "http://localhost:8080/") {
+    APIURL = "http://localhost:3000";
+  }
 }
